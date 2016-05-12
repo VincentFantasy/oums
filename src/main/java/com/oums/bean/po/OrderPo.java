08e9c1e1e8f31dce7944a19e6a722b076685105c
Fantasy <Fantasy@DESKTOP-EQ9UA1N>
2016-05-12 22:50:10 +0800

    修复orderPo

--- src/main/java/com/oums/bean/po/OrderPo.java
+++ src/main/java/com/oums/bean/po/OrderPo.java
@@ -4,18 +4,23 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 /**
  * 订单表，需要加东西的说下
  * @author 谭治
  *
  */
+@SuppressWarnings("deprecation")
+@Entity
+@Table(name="t_order")
 public class OrderPo extends BasePo {
 	/* id */
 	private Integer orderId;
