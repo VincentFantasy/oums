c2fdce41cee67a8c6154b82ebc925652821bb78f
Fantasy <Fantasy@DESKTOP-EQ9UA1N>
2016-05-22 15:12:32 +0800

    更新说明：1增加了vo到po，po到vo的转换，要求vo，po属性名一致，使用很简单 更新日期：2016年5月22日15:12:10 操作人：tz

---
+++ src/main/java/com/oums/bean/vo/OrderVo.java
@@ -0,0 +1,116 @@
+package com.oums.bean.vo;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.oums.bean.po.SitePo;
+import com.oums.bean.po.UserPo;
+
+/**
+ * 订单表，需要加东西的说下
+ * @author 谭治
+ *
+ */
+public class OrderVo extends BaseVo {	
+	/* id */
+	private Integer orderId;
+	/* 订单号 */
+	private String orderNumber;
+	/* 订单类型 */
+	private Integer orderClass;
+	/* 订单状态 */
+	private Integer orderType;
+	/* 订单生成时间 */
+	private Date buildTime;
+	/* 预定开始时间 */
+	private Date startTime;
+	/* 时长 (小时) */
+	private Integer timeOut;
+	/* 是否删除  */
+	private Boolean isDelete;
+	/* 用户 */
+	private UserPo user;
+	/* 场地 */
+	private List<SitePo> siteList = new ArrayList<>();
+	/* 赛事 */
+//	private List<RacePo> raceList = new ArrayList<>();
+	/* 还要加管理员的Po */
+	
+	public Integer getOrderId() {
+		return orderId;
+	}
+	
+	public Integer getOrderClass() {
+		return orderClass;
+	}
+
+	public void setOrderClass(Integer orderClass) {
+		this.orderClass = orderClass;
+	}
+
+	public Integer getOrderType() {
+		return orderType;
+	}
+
+	public void setOrderType(Integer orderType) {
+		this.orderType = orderType;
+	}
+
+	public void setOrderId(Integer orderId) {
+		this.orderId = orderId;
+	}
+	public Boolean getIsDelete() {
+		return isDelete;
+	}
+	public void setIsDelete(Boolean isDelete) {
+		this.isDelete = isDelete;
+	}
+	public String getOrderNumber() {
+		return orderNumber;
+	}
+	public void setOrderNumber(String orderNumber) {
+		this.orderNumber = orderNumber;
+	}
+	public Date getBuildTime() {
+		return buildTime;
+	}
+	public void setBuildTime(Date buildTime) {
+		this.buildTime = buildTime;
+	}
+	public Date getStartTime() {
+		return startTime;
+	}
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
+	public Integer getTimeOut() {
+		return timeOut;
+	}
+	public void setTimeOut(Integer timeOut) {
+		this.timeOut = timeOut;
+	}
+	public UserPo getUser() {
+		return user;
+	}
+	public void setUser(UserPo user) {
+		this.user = user;
+	}
+	public List<SitePo> getSiteList() {
+		return siteList;
+	}
+	public void setSiteList(List<SitePo> siteList) {
+		this.siteList = siteList;
+	}
+	@Override
+	public String toString() {
+		return "OrderPo [orderId=" + orderId + ", orderNumber=" + orderNumber + ", orderClass=" + orderClass
+				+ ", orderType=" + orderType + ", buildTime=" + buildTime + ", startTime=" + startTime + ", timeOut="
+				+ timeOut + ", isDelete=" + isDelete + ", user=" + user + ", siteList=" + siteList + "]";
+	}
+	public OrderVo() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+}
--- src/main/java/com/oums/junit/site/TestSiteService.java
+++ src/main/java/com/oums/junit/site/TestSiteService.java
@@ -2,13 +2,18 @@ package com.oums.junit.site;
 
 import static org.junit.Assert.assertTrue;
 
+import java.lang.reflect.InvocationTargetException;
+
+import org.apache.commons.beanutils.BeanUtils;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.oums.bean.ReturnMessage;
+import com.oums.bean.po.SitePo;
 import com.oums.bean.type.SiteType;
 import com.oums.bean.vo.SiteVo;
 import com.oums.service.ISiteService;
+import com.oums.util.BeanUtil;
 
 public class TestSiteService {
 
@@ -26,5 +31,65 @@ public class TestSiteService {
 		
 		assertTrue(returnMessage.isFlat());
 	}
-
+	
+	@Test
+	public void test2() {
+		SiteVo vo = new SiteVo();
+		vo.setSiteId(123);
+		vo.setSiteName("123");
+		vo.setIsDelete(false);
+		vo.setIsUsing(false);
+		vo.setSiteCost(123f);
+		vo.setSiteDtail("123");
+		vo.setSiteType(1);
+		SitePo po = new SitePo();
+		System.out.println(vo);
+		System.out.println(po);
+		try {
+			BeanUtils.copyProperties(po, vo);
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(vo);
+		System.out.println(po);
+		
+	}
+	
+	@Test
+	public void test3() {
+		SiteVo vo = new SiteVo();
+		vo.setSiteId(123);
+		vo.setSiteName("123");
+		vo.setIsDelete(false);
+		vo.setIsUsing(false);
+		vo.setSiteCost(123f);
+		vo.setSiteDtail("123");
+		vo.setSiteType(1);
+		SitePo po = new SitePo();
+		
+		BeanUtil.voToPo(po, vo);
+		
+		System.out.println(po);
+	}
+	
+	@Test
+	public void test4() {
+		SitePo po = new SitePo();
+		po.setSiteId(123);
+		po.setSiteName("123");
+		po.setIsDelete(false);
+		po.setIsUsing(false);
+		po.setSiteCost(123f);
+		po.setSiteDtail("123");
+		po.setSiteType(1);
+		SiteVo vo = new SiteVo();
+		
+		BeanUtil.poToVo(vo, po);
+		
+		System.out.println(vo);
+	}
 }
